f53aac3e0454533c67661288917b6b2a99181d23
mugukamil <mugukamil@gmail.com>
2017-03-10 13:56:56 +0300

    add two section in index page

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
 //=include lib/jquery.min.js
+//=include lib/slick.min.js
+$(function() {
+
+$('.slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true
+})
+
+$('.news-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    infinite: true
+})
+
+})
