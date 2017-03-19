f9d8a255ab43dcf96d2242c890f3dfaff60fbbe9
mugukamil <mugukamil@gmail.com>
2017-03-19 11:46:53 +0300

    add some pages

--- src/js/app.js
+++ src/js/app.js
@@ -4,50 +4,67 @@
 
 $(function() {
 
-$('.slider').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    infinite: true
-})
-
-$('.slider-six').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-})
-
-$('.slider-four').slick({
-    slidesToShow: 4,
-    slidesToScroll: 1,
-})
-
-$(window).load(function(){
-
-    $('.grid').masonry({
-      itemSelector: '.grid-item',
-      columnWidth: 278,
-      gutter: 20
+    $('.slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        infinite: true
     })
-})
 
-$('.catalog__filter-list').on('click', 'a', function(e) {
-    e.preventDefault()
-    var $this = $(this)
+    $('.slider-six').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+    })
 
-    if ($this.hasClass('down')) {
-        $this.closest('.catalog__filter-list').find('a').removeClass('active up')
-        $this.addClass('active')
-        $this.removeClass('down').addClass('up')
-    } else if ($this.hasClass('up')) {
-        $this.closest('.catalog__filter-list').find('a').removeClass('active down')
-        $this.addClass('active')
-        $this.removeClass('up').addClass('down')
-    } else {
-        $this.closest('.catalog__filter-list').find('a').removeClass('active down up')
-        $this.addClass('active')
-        $this.addClass('down')
-    }
+    $('.slider-four').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+    })
 
-})
+    $('.product-slider__slide').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.product-slider__nav',
+        draggable: false,
+    });
+
+    $('.product-slider__nav').slick({
+        slidesToShow: 8,
+        asNavFor: '.product-slider__slide',
+        enterMode: false,
+        focusOnSelect: true,
+        infinite: false,
+        prevArrow: false,
+        nextArrow: false,
+    });
+
+    $(window).load(function(){
+        $('.grid').masonry({
+            itemSelector: '.grid-item',
+            columnWidth: 278,
+            gutter: 20
+      })
+    })
+
+    $('.catalog__filter-list').on('click', 'a', function(e) {
+        e.preventDefault()
+        var $this = $(this)
+
+        if ($this.hasClass('down')) {
+            $this.closest('.catalog__filter-list').find('a').removeClass('active up')
+            $this.addClass('active')
+            $this.removeClass('down').addClass('up')
+        } else if ($this.hasClass('up')) {
+            $this.closest('.catalog__filter-list').find('a').removeClass('active down')
+            $this.addClass('active')
+            $this.removeClass('up').addClass('down')
+        } else {
+            $this.closest('.catalog__filter-list').find('a').removeClass('active down up')
+            $this.addClass('active')
+            $this.addClass('down')
+        }
+    })
 
 })
